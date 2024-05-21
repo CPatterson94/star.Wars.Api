99934b5cd621ac53b23b05271d23f65725060997
Curtis Patterson <Dpatter94@yahoo.com>
2024-05-21 00:33:22 -0400

    Created Pages for Full Sheet and for Single objet from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./index.css";
-import { useGetAllPeopleQuery } from "./api";
+// import { useGetAllPeopleQuery } from "./api";
+// import { Route, Routes } from "react-router-dom";
 
 function App() {
+  // const { isLoading } = useGetAllPeopleQuery();
+
   return (
     <>
+      {/* <Nav />
+      {isLoading? <div className="loading"><h1>Loading Characters...</h1></div> : <Routes>
+         } */}
       <h1>Hello</h1>
     </>
   );
